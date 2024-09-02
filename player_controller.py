81e69313ef9b34297379929be07c156b05a6a08c
Rohit Dewaliya <136791205+dev-world-rohit@users.noreply.github.com>
2024-09-03 00:40:42 +0530

    Delete player_controller.py

--- player_controller.py
+++
@@ -1,9 +0,0 @@
-import random
-
-class PlayerController:
-    def __init__(self, player1, player2):
-        self.player1 = player1
-        self.player2 = player2
-        self.turn = random.choice([0, 1])
-        self.choice = "O" if self.turn == 1 else "X"
-
